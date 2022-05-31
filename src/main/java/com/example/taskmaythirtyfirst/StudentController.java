fc675089b046303d8cba1334a29e8fc37bf284c1
qlin077 <qingqing.lin@baruchcollege.edu>
2022-06-01 06:33:50 +0800

    added data

--- src/main/java/com/example/taskmaythirtyfirst/StudentController.java
+++ src/main/java/com/example/taskmaythirtyfirst/StudentController.java
@@ -10,24 +10,53 @@ import java.util.List;
 public class StudentController {
 
     @GetMapping("/students")
-    public List<StudentResponse> getStudentResponse() {
-        List<StudentResponse> studentResponse = new ArrayList<>();
-
-        StudentResponse studentResponseOne = new StudentResponse();
-        studentResponseOne.setId(1);
-        studentResponseOne.setName("aaaaa");
-        studentResponseOne.setGrade("3");
-        studentResponseOne.setClassroom("1");
-
-        StudentResponse studentResponseTwo = new StudentResponse();
-        studentResponseTwo.setId(2);
-        studentResponseTwo.setName("bbbbb");
-        studentResponseTwo.setGrade("1");
-        studentResponseTwo.setClassroom("1");
-
-        studentResponse.add(studentResponseOne);
-        studentResponse.add(studentResponseTwo);
+    public StudentResponse getStudentResponse() {
+        List<StudentResponseData> studentResponseData = getStudentResponseData();
+        StudentResponse studentResponse = new StudentResponse();
+        studentResponse.setData(studentResponseData);
 
         return studentResponse;
     }
+
+    private List<StudentResponseData> getStudentResponseData() {
+        List<StudentResponseData> studentResponseData = new ArrayList<>();
+
+        StudentResponseData studentResponseDataOne = new StudentResponseData();
+        studentResponseDataOne.setId(1);
+        studentResponseDataOne.setName("aaaaa");
+        studentResponseDataOne.setGrade("3");
+        studentResponseDataOne.setClassroom("1");
+
+        StudentResponseData studentResponseDataTwo = new StudentResponseData();
+        studentResponseDataTwo.setId(2);
+        studentResponseDataTwo.setName("bbbbb");
+        studentResponseDataTwo.setGrade("1");
+        studentResponseDataTwo.setClassroom("1");
+
+        studentResponseData.add(studentResponseDataOne);
+        studentResponseData.add(studentResponseDataTwo);
+        return studentResponseData;
+    }
+//
+//    @GetMapping("/students")
+//    public List<StudentResponse> getStudentResponse() {
+//        List<StudentResponse> studentResponse = new ArrayList<>();
+//
+//        StudentResponse studentResponseOne = new StudentResponse();
+//        studentResponseOne.setId(1);
+//        studentResponseOne.setName("aaaaa");
+//        studentResponseOne.setGrade("3");
+//        studentResponseOne.setClassroom("1");
+//
+//        StudentResponse studentResponseTwo = new StudentResponse();
+//        studentResponseTwo.setId(2);
+//        studentResponseTwo.setName("bbbbb");
+//        studentResponseTwo.setGrade("1");
+//        studentResponseTwo.setClassroom("1");
+//
+//        studentResponse.add(studentResponseOne);
+//        studentResponse.add(studentResponseTwo);
+//
+//        return studentResponse;
+//    }
 }
--- src/main/java/com/example/taskmaythirtyfirst/StudentResponseData.java
+++ src/main/java/com/example/taskmaythirtyfirst/StudentResponseData.java
@@ -4,11 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class StudentResponse {
-    private List data;
+public class StudentResponseData {
+        private Integer id;
+        private String name;
+        private String grade;
+        private String classroom;
+
 }
+
